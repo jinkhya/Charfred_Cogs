7d054fa80a6ed9e9fae01802a800689f5e482b81
Kella <kellador@gmail.com>
2018-05-28 22:44:18 +0200

    :wrench: improve

--- dataCogs/chartop.py
+++ dataCogs/chartop.py
@@ -1,4 +1,5 @@
 import os
+import functools
 import psutil
 from discord.ext import commands
 from utils.discoutils import permissionNode
@@ -9,20 +10,22 @@ class chartop:
         self.bot = bot
         self.loop = bot.loop
 
-    def getCharInfo():
-        charProc = psutil.Process(os.getpid())
-        with charProc.oneshot():
-            cCPUPerc = charProc.cpu_percent(interval=2)
-            cMemInfo = charProc.memory_info()
-            cMemPerc = charProc.memory_percent()
-        return cCPUPerc, cMemInfo, cMemPerc
+    def getCharInfo(pid):
+        proc = psutil.Process(pid)
+        with proc.oneshot():
+            cpuPerc = proc.cpu_percent(interval=2)
+            memInfo = proc.memory_info()
+            memPerc = proc.memory_percent()
+        return cpuPerc, memInfo, memPerc
 
     @commands.command(invoke_without_command=True, aliases=['chartop'])
     @permissionNode('chartop')
     async def top(self, ctx):
         """Get info on Charfred\'s process!"""
 
-        cpuPerc, mem, memPerc = await self.loop.run_in_executor(None, self.getCharInfo)
+        charPid = os.getpid()
+        charInfo = functools.partial(self.getCharInfo, charPid)
+        cpuPerc, mem, memPerc = await self.loop.run_in_executor(None, charInfo)
 
         msg = [' Charfred Process Information ',
                '==============================',
