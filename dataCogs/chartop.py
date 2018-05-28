55e75432444f4f0a75ed8045c7042243456de9aa
Kella <kellador@gmail.com>
2018-05-28 23:03:14 +0200

    :wrench: make human readable

--- dataCogs/chartop.py
+++ dataCogs/chartop.py
@@ -10,13 +10,24 @@ class chartop:
         self.bot = bot
         self.loop = bot.loop
 
-    def getCharInfo(self, pid):
+    def getProcInfo(self, pid):
         proc = psutil.Process(pid)
         with proc.oneshot():
-            cpuPerc = proc.cpu_percent(interval=2)
+            cpuPerc = proc.cpu_percent(interval=3)
             memInfo = proc.memory_info()
             memPerc = proc.memory_percent()
         return cpuPerc, memInfo, memPerc
+
+    def humanReadable(self, n):
+        symbols = ('K', 'M', 'G', 'T', 'P', 'E', 'Z', 'Y')
+        prefix = {}
+        for i, s in enumerate(symbols):
+            prefix[s] = 1 << (i + 1) * 10
+        for s in reversed(symbols):
+            if n >= prefix[s]:
+                value = float(n) / prefix[s]
+                return '%.1f%s' % (value, s)
+        return "%sB" % n
 
     @commands.command(invoke_without_command=True, aliases=['chartop'])
     @permissionNode('chartop')
@@ -24,16 +35,17 @@ class chartop:
         """Get info on Charfred\'s process!"""
 
         charPid = os.getpid()
-        charInfo = functools.partial(self.getCharInfo, charPid)
+        charInfo = functools.partial(self.getProcInfo, charPid)
         cpuPerc, mem, memPerc = await self.loop.run_in_executor(None, charInfo)
+        memPerc = '%.2f' % memPerc
 
         msg = [' Charfred Process Information ',
                '==============================',
-               f'CPU Percentage:      {cpuPerc}',
-               '-----------------------------',
-               f'Resident Set Size:   {mem.rss}',
-               f'Virtual Memory Size: {mem.vms}',
-               '-----------------------------',
+               f'CPU Percentage:      {cpuPerc}%',
+               ' ',
+               f'Resident Set Size:   {self.humanReadable(mem.rss)}',
+               f'Virtual Memory Size: {self.humanReadable(mem.vms)}',
+               ' ',
                f'Memory Percentage:   {memPerc}']
         msg = '\n'.join(msg)
         await ctx.send(f'```{msg}```')
