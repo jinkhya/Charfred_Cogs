1e7c22b35259d5f96878d1c9fe308c88578bb902
Kella <kellador@gmail.com>
2018-06-17 18:51:51 +0200

    :hammer: attempt workaround for discordpy converter not taking defaults

--- dataCogs/quote.py
+++ dataCogs/quote.py
@@ -3,7 +3,7 @@ import discord
 from random import randrange
 from discord.ext import commands
 from utils.config import Config
-from utils.discoutils import permissionNode
+from utils.discoutils import permissionNode, sendMarkdown
 
 log = logging.getLogger('charfred')
 
@@ -38,7 +38,7 @@ class Quotator:
 
     @commands.group(invoke_without_command=True)
     @permissionNode('quote')
-    async def quote(self, ctx, member: discord.Member=None, _index: int=None):
+    async def quote(self, ctx, member=None, _index: int=None):
         """User Quote operations.
 
         Without a subcommand, this returns a list
@@ -46,37 +46,43 @@ class Quotator:
         quote repository.
         """
 
-        if member and member.id in self.quotes:
-            if _index is None:
-                log.info('Random quote!')
-                _index = randrange(len(self.quotes[member.id]))
-                q = self.quotes[member.id][_index]['quote']
-            else:
-                try:
-                    log.info('Specific quote!')
+        if member:
+            try:
+                member = await commands.MemberConverter().convert(ctx, member)
+            except commands.BadArgument:
+                await sendMarkdown(ctx, 'Given member could not be resolved, sorry!')
+                return
+            if member.id in self.quotes:
+                if _index is None:
+                    log.info('Random quote!')
+                    _index = randrange(len(self.quotes[member.id]))
                     q = self.quotes[member.id][_index]['quote']
-                except (KeyError, IndexError):
-                    log.info('No quote with that index!')
-                    await ctx.send('Sorry sir, there is no quote under that number!')
-                    return
-            if member.nick:
-                name = member.nick
-            else:
-                name = member.name
-            await ctx.send(f'{q}\n\n_{name}; Quote #{_index}_')
-        else:
-
-            converter = commands.MemberConverter()
-
-            async def getName(id):
-                member = await converter.convert(ctx, id)
-                if member.nick:
-                    return member.nick
                 else:
-                    return member.name
+                    try:
+                        log.info('Specific quote!')
+                        q = self.quotes[member.id][_index]['quote']
+                    except (KeyError, IndexError):
+                        log.info('No quote with that index!')
+                        await ctx.send('Sorry sir, there is no quote under that number!')
+                        return
+                if member.nick:
+                    name = member.nick
+                else:
+                    name = member.name
+                await ctx.send(f'{q}\n\n_{name}; Quote #{_index}_')
+                return
 
-            members = [await getName(id) for id in self.quotes.keys()]
-            await ctx.send(f'I have quotes from these members:\n ```\n{members}\n```')
+        converter = commands.MemberConverter()
+
+        async def getName(id):
+            member = await converter.convert(ctx, id)
+            if member.nick:
+                return member.nick
+            else:
+                return member.name
+
+        members = '\n'.join([await getName(id) for id in self.quotes.keys()])
+        await ctx.send(f'I have quotes from these members:\n ```\n{members}\n```')
 
     @quote.command(aliases=['delete', 'unquote'])
     async def remove(self, ctx, member: discord.Member, *, _index: int):
