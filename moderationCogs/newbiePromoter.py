bb16a1e4a7cfa84c650875f6606fc5313480b401
Kella <kellador@gmail.com>
2018-06-19 00:08:37 +0200

    :wrench: fix promote not working for users that havn't joined yet

--- moderationCogs/newbiePromoter.py
+++ moderationCogs/newbiePromoter.py
@@ -1,9 +1,8 @@
 import logging
-import discord
 from discord.ext import commands
 from discord.utils import find
 from utils.config import Config
-from utils.discoutils import permissionNode
+from utils.discoutils import permissionNode, sendMarkdown
 
 log = logging.getLogger('charfred')
 
@@ -18,34 +17,41 @@ class NewbiePromoter:
             self.promotees['awaiting'] = []
 
     async def on_member_join(self, member):
-        if member.id in self.promotees['awaiting']:
+        if str(member) in self.promotees['awaiting']:
             log.info(f'Promotee {member.name} has joined, promoting now.')
             memberRole = find(lambda r: r.name == self.memberRoleName, member.guild.roles)
             await member.add_roles(memberRole)
-            self.promotees['awaiting'].remove(member.id)
+            self.promotees['awaiting'].remove(str(member))
             await self.promotees.save()
 
     @commands.command()
     @commands.guild_only()
     @permissionNode('newbiepromote')
-    async def promote(self, ctx, user: discord.User):
+    async def promote(self, ctx, promotee: str):
         """Promote a user to the baseline member role.
 
+        Takes a username plus discriminator.
         If the user to promote is not already a member,
         he will be promoted upon joining the guild.
         The baseline member role is configurable.
         """
-        member = ctx.guild.get_member(user.id)
-        if member:
+
+        try:
+            member = await commands.MemberConverter().convert(ctx, promotee)
+        except commands.BadArgument:
+            if len(promotee) > 5 and promotee[-5] == '#':
+                log.info('User hasn\'t joined yet, adding to waitlist.')
+                self.promotees['awaiting'].append(promotee)
+                await self.promotees.save()
+                await sendMarkdown(ctx, '# User has not joined yet, adding to waitlist.')
+            else:
+                log.info('Cannot wait for invalid username!')
+                await sendMarkdown(ctx, f'< \"{promotee}\" is in an invalid format, please try again!')
+        else:
             log.info('User is already a member, promoting immediately.')
             memberRole = find(lambda r: r.name == self.memberRoleName, member.guild.roles)
             await member.add_roles(memberRole)
-            await ctx.send('User is already a member, promoting immediately.')
-        else:
-            log.info('User hasn\'t joined yet, adding to waitlist.')
-            self.promotees['awaiting'].append(user.id)
-            await self.promotees.save()
-            await ctx.send('User hasn\'t joined yet, adding to waitlist.')
+            await sendMarkdown(ctx, '# User is already a member, promoting immediately.')
 
 
 def setup(bot):
