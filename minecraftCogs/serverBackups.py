8490d8f2a2bab4fefb63d6c65a4b6e4a6e3d1908
Kella <kellador@gmail.com>
2018-12-09 18:45:05 +0100

    :hammer: herp derp, too stupid to use sort aparently

--- minecraftCogs/serverBackups.py
+++ minecraftCogs/serverBackups.py
@@ -37,7 +37,8 @@ class ServerBackups:
         bpath = self.servercfg['backupspath']
         availablebackups = [archive for archive in os.listdir(f'{bpath}/{server}')
                             if os.path.isfile(f'{bpath}/{server}/{archive}') and
-                            archive.endswith('.tar.gz')].sort()
+                            archive.endswith('.tar.gz')]
+        availablebackups.sort()
         backupsbook = Flipbook(ctx, availablebackups, entries_per_page=8,
                                title=f'Backups for {server}', color=Color.blurple())
         await backupsbook.flip()
