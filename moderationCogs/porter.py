7f30acc9e29216aa3fe2928cbc4ebcc82011c6a2
Kella <kellador@gmail.com>
2019-01-12 20:10:11 +0100

    :bento: extend newbiePromote functionality and rename to Porter

--- moderationCogs/porter.py
+++ moderationCogs/porter.py
@@ -1,4 +1,5 @@
 import logging
+from discord import Forbidden
 from discord.ext import commands
 from discord.utils import find
 from utils.config import Config
@@ -7,12 +8,13 @@ from utils.discoutils import permissionNode, sendMarkdown
 log = logging.getLogger('charfred')
 
 
-class NewbiePromoter:
+class Porter:
     def __init__(self, bot):
         self.bot = bot
         self.promotees = Config(f'{self.bot.dir}/data/promotees_persist.json',
                                 load=True, loop=self.bot.loop)
         self.memberRoleName = bot.cfg['nodes']['spec:memberRole'][0]
+        self.floodmode = False
         if 'awaiting' not in self.promotees:
             self.promotees['awaiting'] = []
 
@@ -23,6 +25,59 @@ class NewbiePromoter:
             await member.add_roles(memberRole)
             self.promotees['awaiting'].remove(str(member))
             await self.promotees.save()
+            return
+
+        if self.floodmode:
+            if member.top_role.name == '@everyone':
+                log.info(f'Floodmode: {member.name} has no pending membership!')
+                try:
+                    await member.kick(reason="No pending membership!")
+                except Forbidden:
+                    log.warning('No permission to kick members!')
+                else:
+                    log.info(f'Floodmode: {member.name} was kicked!')
+                    await member.send(f'Hey there {member.name},\nsorry for kicking you from '
+                                      f'{member.guild.name}, but we are currently in floodmode.'
+                                      '\nIf you have a pending application please wait for it '
+                                      'to be approved and then try joining our Discord again!\n'
+                                      'Good day!\n\n - This is a machine generated message, '
+                                      'replies are futile!')
+
+    @commands.group(invoke_without_command=True)
+    @commands.guild_only()
+    async def floodmode(self, ctx):
+        """Floodmode operations.
+
+        Without a subcommand, this returns the current floodmode status.
+        """
+        if self.floodmode:
+            log.info('Floodmode active!')
+            await sendMarkdown(ctx, '# Floodmode is currently active!')
+        else:
+            log.info('Floodmode inactive!')
+            await sendMarkdown(ctx, '> Floodmode is currently inactive!')
+
+    @floodmode.command(aliases=['start', 'on'])
+    @commands.guild_only()
+    @permissionNode('floodmode')
+    async def activate(self, ctx):
+        """Activates floodmode.
+
+        During floodmode any joining users who have no pending membership
+        will be automatically kicked.
+        """
+
+        log.info('Activated floodmode!')
+        self.floodmode = True
+
+    @floodmode.command(aliases=['stop', 'off'])
+    @commands.guild_only()
+    @permissionNode('floodmode')
+    async def deactivate(self, ctx):
+        """Deactivates floodmode."""
+
+        log.info('Deactivated floodmode!')
+        self.floodmode = False
 
     @commands.command()
     @commands.guild_only()
@@ -55,10 +110,11 @@ class NewbiePromoter:
 
 
 def setup(bot):
-    bot.add_cog(NewbiePromoter(bot))
+    bot.add_cog(Porter(bot))
 
 
 permissionNodes = {
     'spec:memberRole': ['Please enter the name of the baseline member role to be promoted to', "Member"],
-    'newbiepromote': ''
+    'newbiepromote': '',
+    'floodmode': ''
 }
