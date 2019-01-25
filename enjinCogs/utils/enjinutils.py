58adff41b47b69f5257ad38d420dcb45933e5186
Kella <kellador@gmail.com>
2019-01-25 19:53:50 +0100

    :hammer: derp

--- enjinCogs/utils/enjinutils.py
+++ enjinCogs/utils/enjinutils.py
@@ -24,7 +24,7 @@ async def post(clientsession, payload, url):
 
 
 async def login(clientsession, enjinlogin):
-    if enjinlogin is None:
+    if enjinlogin:
         payload = {
             'method': 'User.login',
             'params': {
