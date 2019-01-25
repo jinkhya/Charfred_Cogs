27942984f3e3b82757d9850d69a330cf5b65f443
Kella <kellador@gmail.com>
2019-01-25 19:35:36 +0100

    :bento: add enjin functionality

---
+++ enjinCogs/enjinseer.py
@@ -0,0 +1,120 @@
+import logging
+import json
+from collections import namedtuple
+from discord import Forbidden
+from discord.ext import commands
+from utils.discoutils import send, sendMarkdown, promptInput
+from .utils.enjinutils import login, verifysession, post
+
+log = logging.getLogger('charfred')
+
+Enjinlogin = namedtuple('Enjinlogin', 'email password url site_id')
+
+
+class Enjinseer:
+    def __init__(self, bot):
+        self.bot = bot
+        self.session = bot.session
+        if not hasattr(bot, 'enjinsession'):
+            bot.enjinsession = None
+        self.enjinsession = bot.enjinsession
+        if not hasattr(bot, 'enjinlogin'):
+            bot.enjinlogin = None
+        self.enjinlogin = bot.enjinlogin
+
+    @commands.group()
+    @commands.is_owner()
+    async def enjin(self, ctx):
+        """Enjin Management commands."""
+        pass
+
+    @enjin.command()
+    @commands.is_owner()
+    async def login(self, ctx):
+        """Enjin user login and session saving.
+
+        Interactively requests necessary login information
+        from the user via DM and establishes a session to be
+        used by other enjin related cogs.
+        """
+
+        log.info('Starting enjin login!')
+        if self.enjinsession and self.enjinlogin:
+            valid = await verifysession(self.enjinlogin)
+            if valid:
+                log.info('Current enjin session still valid!')
+                await sendMarkdown(ctx, "# Current enjin session is still valid!")
+                return
+            else:
+                log.info('Current enjin session invalid!')
+        email, _, _ = await promptInput(ctx, '# Beginning Enjin login!\n'
+                                        '# Please enter your login email:')
+        password, _, _ = await promptInput(ctx, '# Please enter your login password:')
+        url, _, _ = await promptInput(ctx, '# Please enter the url for your enjin site:')
+        site_id, _, _ = await promptInput(ctx, '# Please enter the site id for that site:')
+        self.enjinlogin = Enjinlogin(
+            email=email,
+            password=password,
+            url=url,
+            site_id=site_id
+        )
+        async with ctx.typing():
+            log.info('Logging into Enjin...')
+            await sendMarkdown(ctx, '> Logging in...')
+            enjinsession = await login(self.session, self.enjinlogin)
+            if enjinsession:
+                self.enjinsession = enjinsession
+                await sendMarkdown(ctx, '# Login successful!', deletable=False)
+            else:
+                await sendMarkdown(ctx, '< Login failed! >', deletable=False)
+        log.info('Cleaning up login dialog...')
+        try:
+            await ctx.message.delete()
+        except Forbidden:
+            log.warning('No permission to clean up!')
+        else:
+            log.info('Cleaned up!')
+
+    @enjin.command(aliases=['stupify', 'post'])
+    @commands.is_owner()
+    async def request(self, ctx, method: str, *params):
+        """Run an arbitrary post request to the enjin api.
+
+        Some basics, such as jsonrpc and current session id
+        are set automagically! You just have to add the
+        api method as the first parameter, and necessary
+        parameters for that method, in key value pairs,
+        seperated by spaces.
+        """
+
+        if not self.enjinlogin:
+            await sendMarkdown(ctx, '< I am not logged in to enjin yet! >')
+
+        async with ctx.typing():
+            log.info('Running enjin post request...')
+            payload = {
+                'method': method
+            }
+
+            if params:
+                log.info('Parsing parameters...')
+                if not (len(params) % 2 == 0):
+                    log.warning('Uneven number of keys/values!')
+                    await sendMarkdown(ctx, '< Uneven number of keys/values! >')
+                    return
+                params = iter(params)
+                params = dict(list(zip(params, params)))
+                payload['params'] = params
+
+            resp = await post(self.session, payload, self.enjinlogin.url)
+            if resp:
+                log.info('Request successful!')
+                resp = json.dumps(resp, indent=2)
+                await send(ctx, f'```json\n{resp[:1800]}```')
+            else:
+                log.warning('Request failed!')
+                await sendMarkdown(ctx, '< Request failed for some reason! >')
+
+
+def setup(bot):
+    bot.add_cog(Enjinseer(bot))
---
+++ enjinCogs/utils/enjinutils.py
@@ -0,0 +1,77 @@
+import logging
+import secrets
+from collections import namedtuple
+
+log = logging.getLogger('charfred')
+
+Enjinsession = namedtuple('Enjinsession', 'session_id user_id username')
+
+
+async def post(clientsession, payload, url):
+    requestid = secrets.randbelow(60000)
+    payload['jsonrpc'] = '2.0'
+    payload['id'] = requestid
+    async with clientsession.post(f'{url}/api/v1/api.php',
+                                  json=payload) as r:
+        if r.status == 200:
+            content = await r.json()
+            if int(content['id']) != requestid:
+                log.warning('Request ID and response ID do not match! Abort!')
+                return None
+            return content
+        else:
+            return None
+
+
+async def login(clientsession, enjinlogin):
+    if enjinlogin is None:
+        payload = {
+            'method': 'User.login',
+            'params': {
+                'email': enjinlogin.email,
+                'password': enjinlogin.password
+            }
+        }
+        resp = await post(clientsession, payload, enjinlogin.url)
+        if resp:
+            try:
+                result = resp['result']
+                enjinsession = Enjinsession(
+                    session_id=result['session_id'],
+                    user_id=result['user_id'],
+                    username=result['username']
+                )
+            except KeyError:
+                log.error('Enjin login failed, response content malformed!')
+            else:
+                log.info('Enjin login successful!')
+                return enjinsession
+        else:
+            log.warning('Enjin login failed!')
+        return None
+
+
+async def verifysession(clientsession, enjinlogin):
+    if enjinlogin:
+        payload = {
+            'method': 'User.checkSession',
+            'params': {
+                'session_id': enjinlogin.session_id
+            }
+        }
+        resp = await post(clientsession, payload, enjinlogin.url)
+        if resp:
+            try:
+                valid = resp['result']['hasIdentity']
+            except KeyError:
+                log.error('Enjin session verification failed, response content malformed!')
+            else:
+                if valid:
+                    log.info('Enjin session valid!')
+                    return True
+                else:
+                    log.info('Enjin session invalid!')
+                    return False
+        else:
+            log.warning('Enjin session verification failed!')
+        return False
