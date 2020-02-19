a341219092efd10c59806faa4f279dda14c4321d
Kellador <kellador@gmail.com>
2020-02-19 21:00:24 +0100

    :zap: add converters for uuid and minecraft coordinates

--- datacogs/unitconverter.py
+++ datacogs/unitconverter.py
@@ -3,8 +3,9 @@ import logging
 from pint import UnitRegistry, DimensionalityError, DefinitionSyntaxError, \
     UndefinedUnitError
 
+from discord import Embed
 from discord.ext import commands
-from utils.discoutils import sendmarkdown
+from utils.discoutils import sendmarkdown, send
 
 log = logging.getLogger('charfred')
 
@@ -12,6 +13,7 @@ log = logging.getLogger('charfred')
 class UnitConverter(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
+        self.session = bot.session
         self.ur = UnitRegistry()
         self.ur.autoconvert_offset_to_baseunit = True
 
@@ -19,7 +21,7 @@ class UnitConverter(commands.Cog):
     async def convert(self, ctx):
         """Converts stuff.
 
-        Just measurements for now.
+        Just measurements and temperatures for now.
         """
         pass
 
@@ -48,6 +50,50 @@ class UnitConverter(commands.Cog):
                                '< and temperatures. >')
         else:
             await sendmarkdown(ctx, f'# {measurement} is (roughly) {out}')
+
+    @convert.command()
+    async def block(self, ctx, x, z):
+        """Convert Minecraft x, z coordinates to chunk and region.
+        """
+
+        chunk = f'{(x >> 4)}, {(z >> 4)}'
+        regionfile = 'r.' + str((x >> 4) // 32) + '.' + str((z >> 4) // 32) + '.mca'
+        await sendmarkdown(ctx, f'# Coordinates {x}, {z} correspond to:\n'
+                           f'Chunk coordinates: {chunk}\n\n'
+                           f'Region file: {regionfile}')
+
+    @convert.command()
+    async def uuid(self, ctx, uuid: str):
+        """Convert Minecraft UUID to Userprofile Info.
+
+        More of a 'fetch' than a 'convert', since the data isn't actually
+        stored in the UUID, but what the hell...
+        """
+
+        async with self.session.get('https://sessionserver.mojang.com/'
+                                    f'session/minecraft/profile/{uuid}') as r:
+            d = await r.json()
+        if not d:
+            await sendmarkdown(ctx, '< Couldn\'t get anything, sorry! >')
+            return
+        card = Embed(
+            title=f'__Subject: {d["name"]}__',
+            type='rich',
+            color=0xe77070
+        )
+        card.set_thumbnail(
+            url=f'https://crafatar.com/renders/body/{uuid}?overlay'
+        )
+        card.add_field(
+            name="Current Name:",
+            value="```\n" + d["name"] + "\n```"
+        )
+        card.add_field(
+            name="UUID: (hey, you already know this!)",
+            value="```\n" + uuid + "\n```"
+        )
+        card.set_footer(text="Look at that asshole... ಠ_ಠ")
+        await send(ctx, embed=card)
 
 
 def setup(bot):
