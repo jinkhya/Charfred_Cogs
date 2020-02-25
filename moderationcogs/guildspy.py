18f00a040fbe99b0b294bf1c021d7c813cd8ba07
Kellador <kellador@gmail.com>
2020-02-25 23:32:49 +0100

    :art: upgrade to discord.py webhooks

--- moderationcogs/guildspy.py
+++ moderationcogs/guildspy.py
@@ -1,4 +1,5 @@
 import logging
+from discord import Webhook, AsyncWebhookAdapter, Embed
 from discord.ext import commands
 
 log = logging.getLogger('charfred')
@@ -10,47 +11,46 @@ class Guildspy(commands.Cog):
         self.session = bot.session
         self.cfg = bot.cfg
         try:
-            self.hook_url = self.cfg['cogcfgs'][f'{__name__}.spyhook'][0]
-        except:
-            self.hook_url = None
+            self.hook = Webhook.from_url(self.cfg['cogcfgs'][f'{__name__}.spyhook'][0],
+                                         adapter=AsyncWebhookAdapter(self.session))
+        except KeyError:
+            self.hook = None
 
     async def _hookit(self, member, content):
         log.info('Sending spy report!')
-        hook_this = {
-            'username': f'{member.name} via CharSpy',
-            'avatar_url': f'{member.avatar_url}',
-            'content': content
-        }
-        await self.session.post(self.hook_url, json=hook_this)
+        await self.hook.send(
+            username=f'{member.name} via CharSpy',
+            avatar_url=f'{member.avatar_url}',
+            content=content
+        )
 
     @commands.Cog.listener()
     async def on_member_ban(self, guild, user):
         """Ban spy."""
         log.info('Member got banned, ouch!')
-        if self.hook_url:
-            hook_this = {
-                'username': f'{user.name} via CharSpy',
-                'avatar_url': f'{user.avatar_url}',
-                'embeds': [{
-                    'title': 'Ban Action:',
-                    'color': 0x008080,
-                    'image': {
-                        'url': 'https://cdn.discordapp.com/attachments/327174378527653889/535248231265992744/O3DHIA5.gif'
-                    },
-                    'fields': [{
-                        'name': f'I, {user.name}',
-                        'value': 'was banned!'
-                    }]
-                }]
-            }
+        if self.hook:
+            hook_embed = Embed(
+                title='Ban Action:',
+                color=0x008080
+            )
+            hook_embed.set_image('https://cdn.discordapp.com/attachments/'
+                                 '327174378527653889/535248231265992744/O3DHIA5.gif')
+            hook_embed.add_field(
+                name=f'I, {user.name}',
+                value='was banned!'
+            )
             log.info('Sending spy report!')
-            await self.session.post(self.hook_url, json=hook_this)
+            await self.hook.send(
+                username=f'{user.name} via CharSpy',
+                avatar_url=f'{user.avatar_url}',
+                embed=hook_embed
+            )
 
     @commands.Cog.listener()
     async def on_member_join(self, member):
         """Join spy."""
         log.info('Member joined!')
-        if self.hook_url:
+        if self.hook:
             content = f'{member.name}#{member.discriminator} has joined!\nID: {member.id}'
             await self._hookit(member, content)
 
@@ -58,7 +58,7 @@ class Guildspy(commands.Cog):
     async def on_member_remove(self, member):
         """Leave spy."""
         log.info('Member left!')
-        if self.hook_url:
+        if self.hook:
             if member.top_role.name == '@everyone':
                 role = 'nobody'
             else:
@@ -72,7 +72,7 @@ class Guildspy(commands.Cog):
         if len(before.roles) == len(after.roles):
             return
         log.info('Member has updated roles.')
-        if self.hook_url:
+        if self.hook:
             if len(before.roles) > len(after.roles):
                 roles = list(set(before.roles) - set(after.roles))
                 verb = 'lost'
